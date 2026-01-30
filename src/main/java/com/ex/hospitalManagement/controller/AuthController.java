73ad81cb577c2ea67c47d39d855aa032af7b89af
m1101 <madhavkalal1101@gmail.com>
2026-01-30 21:25:45 +0530

    Implement signup/in with JWT and globalException Handing

---
+++ src/main/java/com/ex/hospitalManagement/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.ex.hospitalManagement.controller;
+
+import com.ex.hospitalManagement.dto.LoginRequestDto;
+import com.ex.hospitalManagement.dto.LoginResponseDto;
+import com.ex.hospitalManagement.dto.SignupResponseDto;
+import com.ex.hospitalManagement.security.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+        return ResponseEntity.ok(authService.login(loginRequestDto));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<SignupResponseDto> signup(@RequestBody LoginRequestDto signupRequestDto) {
+        return ResponseEntity.ok(authService.signup(signupRequestDto));
+    }
+}
---
+++ src/main/java/com/ex/hospitalManagement/security/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.ex.hospitalManagement.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.
+        csrf(csrfConfig -> csrfConfig.disable())
+                .sessionManagement(sessionConfig ->
+                        sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**" , "/auth/**").permitAll()
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+//                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
